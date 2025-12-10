e21251841111c1c2d568b67211ef27770d84634e
Patrik Bednář <patrikbednar06@gmail.com>
2025-12-10 21:17:37 +0100

    adventofcode

--- 02_GiftShop/02_GiftShop/Program.cs
+++ 02_GiftShop/02_GiftShop/Program.cs
@@ -10,15 +10,47 @@ namespace _02_GiftShop
     {
         static void Main(string[] args)
         {
-            string input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+            string input = "6161588270-6161664791,128091420-128157776,306-494,510-1079,10977-20613,64552-123011,33-46,28076-52796,371150-418737,691122-766624,115-221,7426210-7504719,819350-954677,7713444-7877541,63622006-63661895,1370-1981,538116-596342,5371-8580,8850407-8965070,156363-325896,47-86,452615-473272,2012-4265,73181182-73335464,1102265-1119187,3343315615-3343342551,8388258268-8388317065,632952-689504,3-22,988344-1007943";
 
-            string[] id = input.Split(',','-');
+            //rozdeleni inputu na jednotlive rozmezi id
+            string[] ids = input.Split(',');
+            long pocet = ids.Length;
+
+            Console.WriteLine(CheckForRepeating(ids, pocet));
+            Console.ReadLine();
         }
-        static void CheckForRepeating(int id)
+        static long CheckForRepeating(string[] ids, long pocet)
         {
-            string idString = id.ToString();
-            int delka = idString.Length;
-            int cislo
+            long vysledek = 0;
+
+            for (long i = 0; i < pocet; i++)
+            {
+                //rozdeleni rozmezi na cisla od - do
+                string[] id = ids[i].Split('-');
+                long idAlong = long.Parse(id[0]);
+                long idBlong = long.Parse(id[1]);
+
+                long cislo = idAlong;
+                while(cislo <= idBlong)
+                {
+                    int delkaCisla = cislo.ToString().Length;
+                    
+                    if (delkaCisla % 2 == 0)
+                    {
+                        //rozdeleni cisla na dve casti pokud je sude
+                        string levaCast = cislo.ToString().Substring(0, delkaCisla / 2);
+                        string pravaCast = cislo.ToString().Substring(delkaCisla / 2);
+
+                        //kontrola stejnosti obou casti cisla
+                        if (levaCast == pravaCast)
+                        {
+                            vysledek = vysledek + cislo;
+                        }
+                    }
+                    cislo++;
+                }
+            }
+            return vysledek;
         }
     }
 }
---
+++ 03_Lobby/03_Lobby/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _03_Lobby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFile = "..\\..\\battery.txt";
+            using (StreamReader sr = new StreamReader(inputFile))
+            {
+                string line;
+                long vysledek = 0;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //Console.WriteLine(line);
+                    vysledek = vysledek + Joltage(line);
+                }
+                Console.WriteLine(vysledek);
+            }
+            Console.ReadLine();
+
+            string baterka1 = "987654321111111";
+            string baterka2 = "811111111111119";
+            string baterka3 = "234234234234278";
+            string baterka4 = "818181911112111";
+
+            Console.WriteLine(Joltage(baterka1));
+            Console.WriteLine(Joltage(baterka2));
+            Console.WriteLine(Joltage(baterka3));
+            Console.WriteLine(Joltage(baterka4));
+
+            Console.ReadLine();
+        }
+
+        static long Joltage(int[] pole)
+        {
+            
+        }
+    }
+}
